371c52de819698b0b5db51d5318295634de09f27
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2023-01-19 13:33:13 +0100

    feat: styling login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 import * as Animatable from "react-native-animatable";
 import { useNavigation } from "@react-navigation/native";
 
@@ -13,18 +20,58 @@ export default function Login() {
         delay={500}
         style={styles.containerHeader}
       >
-        <Text>Bem-Vindo(a)</Text>
+        <Text style={styles.message}>Bem-Vindo(a)</Text>
       </Animatable.View>
-      <Animatable.View
-        animation="fadeInUp"
-        style={styles.containerForm}
-      ></Animatable.View>
+      <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+        <Text style={styles.title}>Email</Text>
+        <TextInput placeholder="Digide o seu email" style={styles.input} />
+
+        <Text style={styles.title}>Senha</Text>
+        <TextInput placeholder="Sua senha" style={styles.input} />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.buttonRegister}>
+          <Text style={styles.registerText}>
+            Não possui uma conta? Cadastr-se
+          </Text>
+        </TouchableOpacity>
+      </Animatable.View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
-  containerHeader: {},
-  containerForm: {},
+  container: {
+    flex: 1,
+    backgroundColor: "#38A69D",
+  },
+  containerHeader: {
+    marginTop: "14%",
+    marginBottom:"8%",
+    paddingStart: "5%"
+  },message:{
+   fontSize:28,
+   fontWeight:'bold',
+   color: '#fff'
+  },
+  containerForm: {
+    backgroundColor:"#fff", 
+    flex:1 ,
+    borderTopLeftRadius:25 ,
+    borderTopRightRadius: 25 ,
+    paddingStart: "5%", 
+    paddingEnd: "5%"
+  },
+  title: {
+    fontSize: 20, 
+    marginTop: 28
+  },
+  input: {},
+  buttonText: {},
+  button: {},
+  registerText: {},
+  buttonRegister: {},
 });
